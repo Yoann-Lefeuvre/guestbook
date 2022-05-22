e88ecfd91da7ce1fc7944bfffea91c4150dd047b
Yoann Lefeuvre <yoann.lefeuvre@gmail.com>
2022-05-22 19:47:16 +0200

    Ajouter un slug unique

--- migrations/Version20220522164429.php
+++ migrations/Version20220522164429.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220522162029 extends AbstractMigration
+final class Version20220522164429 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20220522162029 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255)');
-        $this->addSql("UPDATE conference SET slug=CONCAT(LOWER(city), '-', year)");
-        $this->addSql('ALTER TABLE conference ALTER COLUMN slug SET NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_911533C8989D9B62 ON conference (slug)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE conference DROP slug');
+        $this->addSql('DROP INDEX UNIQ_911533C8989D9B62 ON conference');
     }
 }
