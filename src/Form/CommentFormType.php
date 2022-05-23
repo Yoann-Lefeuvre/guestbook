162b1448acf1b6e9566300954e24bbe52a1b9a4f
Yoann Lefeuvre <yoann.lefeuvre@gmail.com>
2022-05-23 23:30:37 +0200

    transfert BDD des données formulaire

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -4,20 +4,33 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
+            // ->add('author')
+            // ->add('email')
+            // ->add('createdAt')
+            // ->add('photoFilename')
+            //->add('conference')
+
+            ->add('author', null, [
+                 'label' => 'Your name',])
             ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('photoFilename')
-            ->add('conference')
+            ->add('email', EmailType::class)
+            ->add('photo', FileType::class, [
+                'required' => false,
+                'mapped' => false, // mapped = relié à l'entité Comment
+                'constraints' => [new Image(['maxSize' => '1024k'])],])
+            ->add('submit', SubmitType::class)
         ;
     }
 
