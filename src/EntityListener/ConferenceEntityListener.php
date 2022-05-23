c96eebd6060b2be273af135e60296ad56183d29e
Yoann Lefeuvre <yoann.lefeuvre@gmail.com>
2022-05-23 15:56:08 +0200

    Maj templates for slug

--- src/EntityListener/ConferenceEntityListener.php
+++ src/EntityListener/ConferenceEntityListener.php
@@ -4,7 +4,7 @@ namespace App\EntityListener;
 
 use App\Entity\Conference;
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Symfony\Component\string\Slugger\SluggerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ConferenceEntityListener
 {
