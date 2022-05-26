c7014305a681f4f74c4cb4483345b4865068b036
Yoann Lefeuvre <yoann.lefeuvre@gmail.com>
2022-05-26 17:36:49 +0200

    Add code verif API Akismet ConfControl

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use App\SpamChecker;
 
 class ConferenceController extends AbstractController
 {
@@ -34,7 +35,7 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name:'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir) : Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, SpamChecker $spamChecker, string $photoDir) : Response
     {     
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -52,6 +53,18 @@ class ConferenceController extends AbstractController
                                     }
         
                     $this->entityManager->persist($comment);
+                    
+                    //Identification du SPAM dans les commentaires
+                    $context = [
+                                        'user_ip' => $request->getClientIp(),
+                                        'user_agent' => $request->headers->get('user-agent'),
+                                        'referrer' => $request->headers->get('referer'),
+                                        'permalink' => $request->getUri(),
+                                    ];
+                                    if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                                        throw new \RuntimeException('Blatant spam, go away!');
+                                    }
+
                     $this->entityManager->flush();
         
                     return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
