9afc917da9ff1fc7ba526d032f6eb825338f007c
yoann <yoann.lefeuvre@gmail.com>
2024-09-05 10:05:28 +0200

    navbar responsive 09-2024

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -4,12 +4,14 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentFormType extends AbstractType
 {
@@ -24,7 +26,7 @@ class CommentFormType extends AbstractType
 
             ->add('author', null, [
                  'label' => 'Your name',])
-            ->add('text')
+            ->add('text',TextareaType::class)
             ->add('email', EmailType::class)
             ->add('photo', FileType::class, [
                 'required' => false,
